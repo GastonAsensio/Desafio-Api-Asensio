9b485e606609fdd5840a898cf2639dc56e27b7e1
Gaston Asensio <gastiasensio@gmail.com>
2022-11-29 20:12:51 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const PORT = 8080;
+const app = express();
+const productsRouter = require("./routes/routes-products.js");
+
+app.use(express.static("public"));
+app.use(express.json());
+app.use("/api/productos", productsRouter);
+
+app.listen(PORT, () => {
+  console.log(`>>>>>  Server Up! In port: ${PORT} <<<<<`);
+});
